3090fe3e4353f8644e90f580e368f4f5a728e24c
tundraka <tundraka@gmail.com>
2015-09-27 22:39:23 -0500

    Notes and changes to plo1.R

--- classes/ExploratoryDataAnalysis/assignment/W03/plot1.R
+++ classes/ExploratoryDataAnalysis/assignment/W03/plot1.R
@@ -9,43 +9,10 @@ NEI$type <- as.factor(NEI$type)
 pmSumByYear <- NEI[,.(totalEmissions=sum(Emissions)), .(year)]
 plot(pmSumByYear$year, pmSumByYear$totalEmissions, type='l')
 
-# plot2.R
-baltimore <- NEI[fips=="24510"]
-totsBaltimore <- baltimore[,.(totalEmissions=sum(Emissions)), .(year)]
-with(totsBaltimore, plot(year, totalEmissions, type='l'))
+dev.copy(png, 'plot1.png')
+dev.off()
 
-# plot3.R
-totsBaltimore <- baltimore[,.(totalEmissions=sum(Emissions)), .(year, type)]
-totsBaltimore[,totRoundEmi:=round(totalEmissions, digits=2)]
-
-library(ggplot2)
-ggplot(totsBaltimore, aes(year, totRoundEmi)) + geom_line() + facet_grid(type ~ .)
-
-# plot4.R
-coal <- SCC[EI.Sector %like% 'Coal']
-coalTotEmi <- NEI[SCC %in% coal$SCC,.(totEmi=sum(Emissions)), .(year)]
-coalTotEmiBySCC <- NEI[SCC %in% coal$SCC, .(totEmi=sum(Emissions)), .(year, SCC)]
-
-# Trying to find out trends upward in any of the coal burning.
-upScc <- coalTotEmiBySCC[,.(max(totEmi)), .(SCC, year)]
-upSccs <- upScc[year==2008,.(SCC)]
-
-ggplot(coalTotEmi, aes(year, totEmi)) + geom_line()
-ggplot(coalTotEmiBySCC, aes(year, totEmi)) + geom_line(aes(color = SCC))
-ggplot(coalTotEmiBySCC, aes(year, totEmi)) + geom_line(aes(color = SCC)) + coord_cartesian(ylim = c(0, 1.0e+05))
-ggplot(coalTotEmiBySCC, aes(year, totEmi)) + geom_line(aes(color = SCC)) + coord_cartesian(ylim = c(0, 12500))
-
-# plot5.R
-vehicleSCC <- SCC[EI.Sector %like% 'Vehicle', .(SCC)]
-baltimore <- NEI[fips=="24510" & SCC %in% vehicleSCC$SCC, .(totEmi=sum(Emissions)), .(year)]
-ggplot(baltimore, aes(year, totEmi)) + geom_line()
-
-# plot6.R
-vehicleSCC <- SCC[EI.Sector %like% 'Vehicle', .(SCC)]
-cities <- NEI[(fips=='24510'|fips=='06037') & SCC %in% vehicleSCC$SCC, .(totEmi=sum(Emissions)), .(year, SCC, fips)]
-ggplot(cities, aes(year, totEmi)) + geom_line(aes(color=fips))
-
-trendUp <- function(sccValues) {
-    print(sccValues)
-    coef(lm(year ~ totEmi, sccValues))['totEmi'] > 0
-}
+#trendUp <- function(sccValues) {
+    #print(sccValues)
+    #coef(lm(year ~ totEmi, sccValues))['totEmi'] > 0
+#}
